6fdff782a7c944d961516bee514ba08a4bfb815a
Esen888 <esensharsheev808@gmail.com>
2022-10-19 20:30:48 +0600

    practice7

---
+++ practice7.dart
@@ -0,0 +1,57 @@
+void main() {
+//Task1
+// int i = 1;
+//   while (i<= 100)
+//   {
+//     print(i);
+//     i++;
+//   }
+
+// for (int i=1; i <= 100; i++)
+// {
+// print(i);
+// }
+
+//Task2
+// int i = 11;
+//   while (i<= 33)
+//   {
+//     print(i);
+//     i++;
+//   }
+// for (int i=11; i <= 33; i++)
+// {
+// print(i);
+// }
+
+//Task3
+
+// for (int i=0; i <= 100; i++)
+// {
+//   if (i.isEven) {
+// print(i);
+// }
+// i++;
+// }
+
+
+  // int i = 0;
+  // while (i <= 100) {
+  //   if (i.isEven) { // if(i % 2 == 0)
+  //     print(i);
+  //   }
+
+  //   i++;
+  // }
+
+//task4
+int i = 1;
+int summ = 0;
+  while (i<= 100)
+  {
+  i++;
+  summ = summ+ i;
+  print(i);
+  }
+  print(summ);
+}
